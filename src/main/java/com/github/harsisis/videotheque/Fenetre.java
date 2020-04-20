7977b7788481d40109032dd4313172dbb147aed4
Harsisis <56965488+Harsisis@users.noreply.github.com>
2020-04-20 18:03:12 +0200

    button change the label text

--- src/main/java/com/github/harsisis/videotheque/Fenetre.java
+++ src/main/java/com/github/harsisis/videotheque/Fenetre.java
@@ -2,12 +2,16 @@ package com.github.harsisis.videotheque;
 
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 public class Fenetre extends JFrame{
     private JPanel panel = new JPanel();
     private JPanel title = new JPanel();
-    private JButton ajouterClient = new JButton("Ajouter un client");
+    private JPanel workPlace = new JPanel();
+    private JButton addClient = new JButton("Ajouter un client");
     private JLabel labelApp = new JLabel("Vidéothèque");
+    private JLabel labelClient = new JLabel("client");
 
 
     public Fenetre(){
@@ -16,15 +20,36 @@ public class Fenetre extends JFrame{
         this.setLocationRelativeTo(null);
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
+        addClient.addActionListener(new addClientListener());
+
+        Font montserrat = new Font("Montserrat", Font.BOLD, 18);
+        labelApp.setFont(montserrat);
+
+        workPlace.add(labelClient);
+
         title.setBackground(Color.LIGHT_GRAY);
+        title.setBorder(BorderFactory.createMatteBorder(0, 0, 3, 0, Color.black));
+        title.add(labelApp);
+
         panel.setBackground(Color.WHITE);
         panel.setLayout(new BorderLayout());
-        title.add(labelApp);
         panel.add(title, BorderLayout.NORTH);
-        panel.add(ajouterClient, BorderLayout.SOUTH);
+        panel.add(addClient, BorderLayout.SOUTH);
+        panel.add(workPlace, BorderLayout.CENTER);
 
         this.setContentPane(panel);
         //this.setContentPane(new Panel());
         this.setVisible(true);
+        go();
     }
+
+    private void go() {
+
+    }
+    class addClientListener implements ActionListener{
+        public void actionPerformed(ActionEvent arg0) {
+            labelClient.setText("ici le client");
+        }
+    }
+
 }
