5f532af61e857f64004ac482a8d9a3605e9d7225
melocotton <w4termelon@gmail.com>
2020-04-20 12:37:30 +0200

    adding project specifications + empty test class

--- src/test/java/com/github/harsisis/videotheque/VideothequeTest.java
+++ src/test/java/com/github/harsisis/videotheque/VideothequeTest.java
@@ -1,38 +1,6 @@
-//package com.github.harsisis;
+package com.github.harsisis.videotheque;
 
-//import org.junit.Ignore;
-//import org.junit.Test;
+public class VideothequeTest {
 
-//import static org.junit.Assert.assertFalse;
-//import static org.junit.Assert.assertTrue;
-
-//public class VideothequeTest {
-
-
-//    @Test
-//    @Ignore
-//    public void given_validClient_when_ajoutClient_Then_listOfClientNotEmpty() {
-//        Videotheque videotheque = new Videotheque();
-//        Client aClient = new Fidele(1, "", "");
-//
-//        boolean result = videotheque.ajoutClient(aClient);
-//
-//
-//        assertFalse(videotheque.getClientList().isEmpty());
-//        assertTrue(result);
-//    }
-//
-//    @Test
-//    @Ignore
-//    public void given_existingClient_when_ajoutClient_with_sameClient_Then_returnFalse() {
-//        Videotheque videotheque = new Videotheque();
-//        Client aClient = new Fidele(1, "", "");
-//        videotheque.ajoutClient(aClient);
-//
-//        Client sameClient = new Fidele(1, "", "");
-//
-//        boolean result = videotheque.ajoutClient(sameClient);
-//
-//        assertFalse(result);
-//    }
-//}
+    //TODO : add tests when Videotheque class will implement features
+}
