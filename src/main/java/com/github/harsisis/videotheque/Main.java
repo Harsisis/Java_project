167e2eb4f9dccf7851e1fe9441fccdb57f85f207
Harsisis <56965488+Harsisis@users.noreply.github.com>
2020-04-20 13:45:13 +0200

    test création BD dans le main

--- src/main/java/com/github/harsisis/videotheque/Main.java
+++ src/main/java/com/github/harsisis/videotheque/Main.java
@@ -6,26 +6,6 @@ package com.github.harsisis.videotheque;
 
 public class Main {
     public static void main(String[] args) {
-        CD hello = new CD ("Hello", 3, 2000);
-        System.out.println(hello);
-
-        Client dm = new Client("DUPONT", "Martin", true);
-        System.out.println(dm);
-
-/*
-        DVD lion = new DVD (2,"Lion King 2019", 4, "Jon Favreau");
-        System.out.println(lion);
-
-        Dictionnaire wembley = new Dictionnaire (3,"Good Wembley DICtionnary",2,"Anglais");
-        System.out.println(wembley);
-
-        Livre mockingbird = new Livre (4,"To kill a mocking bird",8, "Harper Lee");
-        System.out.println(mockingbird);
-
-        Fidele martin =new Fidele(1, "Martin", "Lucie");
-        System.out.println(martin);
-
-        Occasionnel dupont =new Occasionnel(2, "Dupont", "Jean");
-        System.out.println(dupont);*/
+        Livre l01 = new Livre("Garfield", 2.5, "Martin", CategorieLivre.BD);
     }
 }
