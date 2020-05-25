ceb209277fe86e97c2feb6e1826084686ae90482
yumamri <irma.houversing@gmail.com>
2020-05-25 17:35:26 +0200

    testing a function

---
+++ src/main/java/com/github/harsisis/videotheque/iu/StockComboBoxRenderer.java
@@ -0,0 +1,22 @@
+package com.github.harsisis.videotheque.iu;
+
+import com.github.harsisis.videotheque.domaine.Produit;
+import com.github.harsisis.videotheque.domaine.Videotheque;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class StockComboBoxRenderer extends DefaultListCellRenderer {
+    public Component getListCellRendererComponent(
+            JList list,
+            Object value,
+            int index,
+            boolean isSelected,
+            boolean cellHasFocus) {
+        if (value instanceof Produit) {
+            value = ((Produit)value)get();
+        }
+        super.getListCellRendererComponent(list, value, index, isSelected, cellHasFocus);
+        return this;
+    }
+}
