1731ad6b814cbee75681337cc0519a26363e3af2
yumamri <irma.houversing@gmail.com>
2020-05-25 22:31:15 +0200

    added a libelle to CategorieLivre

--- src/main/java/com/github/harsisis/videotheque/domaine/CategorieLivre.java
+++ src/main/java/com/github/harsisis/videotheque/domaine/CategorieLivre.java
@@ -1,7 +1,17 @@
 package com.github.harsisis.videotheque.domaine;
 
 public enum CategorieLivre {
-    BD,
-    MANUEL,
-    ROMAN
+    BD("Bande déssinée"),
+    MANUEL("Manuel"),
+    ROMAN("Roman");
+
+    private final String libelle;
+
+    CategorieLivre(String libelle) {
+        this.libelle = libelle;
+    }
+
+    public String getLibelle() {
+        return libelle;
+    }
 }
