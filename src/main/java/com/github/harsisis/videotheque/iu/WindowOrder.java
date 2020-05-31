52847aeb7011b6b4231d98c4f0f6f350f439be56
yumamri <irma.houversing@gmail.com>
2020-06-01 00:36:01 +0200

    added total function

--- src/main/java/com/github/harsisis/videotheque/iu/WindowOrder.java
+++ src/main/java/com/github/harsisis/videotheque/iu/WindowOrder.java
@@ -82,40 +82,17 @@ public class WindowOrder extends JFrame {
 
         DefaultTableModel modelEmprunt = new DefaultTableModel();
 
+        ListSelectionModel cellSelectionModel;
+
         JTable tableEmprunt = new JTable(modelEmprunt);
 
         defineEmpruntTable(modelEmprunt);
+
         defineCommandeTable(modelCommande, tableCommande);
 
-        tableCommande.getSelectionModel().addListSelectionListener(new ListSelectionListener() {
-            @Override
-            public void valueChanged(ListSelectionEvent e) {
-                int lsm = ListSelectionModel.SINGLE_SELECTION;
-                double total = 0;
-                boolean isAdjusting = e.getValueIsAdjusting();
-
-                //on est trop con, la liste emprunts est vide et on ne la rempli pas, forcément que ca n'affiche rien
-
-                if(!isAdjusting){
-                    Client client = (Client) tableCommande.getValueAt(tableCommande.getSelectedRow(),1);
-                    Commande commande = (Commande) tableCommande.getValueAt(tableCommande.getSelectedRow(), 0);
-                    double coefPrix = client.isFidele() ? 1 - Videotheque.REDUC_FIDELE : 1;
-
-                    // updtade liste emprunt
-                    emprunts= commande.getListEmprunt();
-
-                    for (Emprunt emprunt : emprunts) {
-                        System.out.println("tota1l"+trouverProdId(emprunt.getProduitId(), (Iterable<Produit>) Videotheque.getInstance().getListStockProduit().keySet().iterator()).getTarifJournalier());
-                        System.out.println("total2"+emprunt.getDureeLocation());
-                        System.out.println("total3"+coefPrix);
-
-                        total += trouverProdId(emprunt.getProduitId(), (Iterable<Produit>) Videotheque.getInstance().getListStockProduit().keySet().iterator()).getTarifJournalier() * emprunt.getDureeLocation() * coefPrix;
-                    }
-                    System.out.println("total"+total);
-                    amountLbl.setText("Total : " + total);
-                }
-            }
-        });
+        tableCommande.setCellSelectionEnabled(true);
+        cellSelectionModel = tableCommande.getSelectionModel();
+        cellSelectionModel.setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
 
         //buttons on the main page
         cancelOrderBtn.addActionListener(e -> dispatchEvent(new WindowEvent(this, WindowEvent.WINDOW_CLOSING)));
@@ -153,6 +130,20 @@ public class WindowOrder extends JFrame {
             }
         });
 
+        cellSelectionModel.addListSelectionListener(listSelectionEvent -> {
+            Client client = (Client) tableCommande.getValueAt(tableCommande.getSelectedRow(),1);
+            String commandeId = (String) tableCommande.getModel().getValueAt(tableCommande.getSelectedRow(),0);
+            Commande commande = trouverCommande(commandeId);
+            double coefPrix = client.isFidele() ? 1 - Videotheque.REDUC_FIDELE : 1;
+            double total = 0;
+            for (Emprunt emprunt : commande.getListEmprunt()) {
+                double tarif = trouverProdId(emprunt.getProduitId(), (Iterable<Produit>) Videotheque.getInstance().getListStockProduit().keySet().iterator()).getTarifJournalier();
+                int dureeLocation = emprunt.getDureeLocation();
+                total += tarif * dureeLocation * coefPrix;
+
+            }
+            System.out.println(total);
+        });
         //buttons on the delete loaning page
         minusProductBtn.addActionListener(e -> {
             for (Emprunt emp : emprunts) {
@@ -198,6 +189,13 @@ public class WindowOrder extends JFrame {
         return null;
     }
 
+    public Commande trouverCommande(String id) {
+        for(Commande commande : Videotheque.getInstance().getListCommande()) {
+            if (commande.getCommandeId().equals(id))
+                return commande;
+        }
+        return null;
+    }
     private void defineEmpruntTable(DefaultTableModel modelEmprunt) {
         modelEmprunt.addColumn("ID emprunt");
         modelEmprunt.addColumn("Produit");
