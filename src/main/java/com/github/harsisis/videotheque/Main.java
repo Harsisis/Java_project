aacf173d9b9d6c7bf9299127f1ffe1d7f8a2d3f6
yumamri <irma.houversing@gmail.com>
2020-06-13 14:18:20 +0200

    the stock should change when we're adding emprunts or removing emprunts.

--- src/main/java/com/github/harsisis/videotheque/Main.java
+++ src/main/java/com/github/harsisis/videotheque/Main.java
@@ -21,9 +21,5 @@ public class Main {
     public static void main(String[] args) {
         //create window
         WindowHome video = new WindowHome();
-        DVD dvd = new DVD("Leo", 5, "death");
-        Videotheque.getInstance().ajoutClient("NOM", "Prénom", false);
-        Videotheque.getInstance().ajoutProduit(dvd.getProduitId(),dvd);
-        Videotheque.getInstance().ajoutStockProduit(dvd.getProduitId(),4);
     }
 }
